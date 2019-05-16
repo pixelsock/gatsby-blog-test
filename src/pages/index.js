02b15f3f5187185b8c0947c9083032a8fec94ae9
pixelsock <nick@pixelsock.com>
2019-05-16 10:19:30 -0400

    netlify ready commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,22 @@
 import React from 'react'
+import { Link } from 'gatsby'
+
+
 
 import Layout from '../components/layout'
+import Head from '../components/head'
 
 const IndexPage = () => {
   return (
 
     <Layout>
+      <Head title="Home" />
       <h1>Hello,</h1>
       <h2> I'm Nick, a full-stack developer living in beautiful Charlotte.</h2>
+      <p> Need an agency? Call</p>
+      <p><Link to="/contact">Saturday Brand Communications</Link></p>
     </Layout>
-    
+
   )
 }
 
