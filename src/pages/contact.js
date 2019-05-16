c4bb429a03d86db24e6d90ceee9298231ed976fd
pixelsock <nick@pixelsock.com>
2019-05-16 11:40:15 -0400

    Added a contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -9,6 +9,17 @@ const ContactPage = () => {
     <Layout>
       <Head title="Contact Us" />
       <p>Here is some contact information for my site.</p>
+      <form name="contact" netlify>
+        <p>
+          <label>Name <input type="text" name="name" /></label>
+        </p>
+        <p>
+          <label>Email <input type="email" name="email" /></label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
     </Layout>
   )
 }
